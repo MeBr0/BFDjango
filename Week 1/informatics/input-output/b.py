ce771ca8af4db3dedde2e453d97cb51bba6eeb3e
mebr0 <aza17e@gmail.com>
2020-01-15 00:15:08 +0600

    Do informatics input-output

---
+++ Week 1/informatics/input-output/b.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    a = int(input())
+
+    print('The next number for the number {} is {}.'.format(a, a + 1))
+    print('The previous number for the number {} is {}.'.format(a, a - 1))
+
+
+if __name__ == '__main__':
+    main()
