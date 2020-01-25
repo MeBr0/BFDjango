3444194b32a7401bb4578c4d07f433e62a7addcb
mebr0 <aza17e@gmail.com>
2020-01-25 12:42:37 +0600

    Update .gitignore (again)

--- Week 2/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/mebro/Documents/KBTU/6semester/Backend Framework (Django)/Repo/Week 2/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
